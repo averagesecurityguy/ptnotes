0d98318c2428250f064ad211be1094d11d2dad28
averagesecurityguy <stephen@averagesecurityguy.info>
2016-11-16 15:19:09 -0500

    Remove link from port-based attacks.

--- ptn/database.py
+++ ptn/database.py
@@ -278,7 +278,7 @@ class ScanDatabase(Database):
 
             stmt = "SELECT DISTINCT ip FROM items WHERE port=? AND protocol=?"
             if self.execute_sql(stmt, (port, protocol)) is True:
-                return ['{0}:{1}'.format(i['ip'], i['ip']) for i in self.cur.fetchall()]
+                return ['{0}:{1}'.format('', i['ip']) for i in self.cur.fetchall()]
             else:
                 return []
 
